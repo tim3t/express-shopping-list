3e1d50dc547e1661134a4b8e3b12f64ba60f34f6
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-09-25 19:44:15 -0400

    added final routes

--- routes/items.js
+++ routes/items.js
@@ -20,5 +20,40 @@ router.post('', (req, res, next) => {
 		return next(e);
 	}
 });
+router.post('', (req, res, next) => {
+	try {
+		let newItem = new Item(req.body.name, req.body.price);
+		return res.json({ item: newItem });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.get('/:name', (req, res, next) => {
+	try {
+		let foundItem = Item.find(req.params.name);
+		return res.json({ item: foundItem });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.patch('/:name', (req, res, next) => {
+	try {
+		let foundItem = Item.update(req.params.name, req.body);
+		return res.json({ item: foundItem });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.delete('/:name', (req, res, next) => {
+	try {
+		Item.remove(req.params.name);
+		return res.json({ message: 'Deleted' });
+	} catch (err) {
+		return next(err);
+	}
+});
 
 module.exports = router;
